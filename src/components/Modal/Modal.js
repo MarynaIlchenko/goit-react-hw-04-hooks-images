74428e3c86f9a6919480a5cf4542f7a9ddd3312c
MarynaIlchenko <debovamail@gmail.com>
2022-06-05 01:33:17 +0300

    update

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,48 +1,40 @@
 import PropTypes from 'prop-types';
-import React, { Component } from 'react';
+import { useEffect } from 'react';
 import { createPortal } from 'react-dom';
 import style from './Modal.module.css';
 
 const modalRoot = document.querySelector('#modal-root');
 
-export class Modal extends Component {
-  static = {
-    onToggleModal: PropTypes.func.isRequired,
-    img: PropTypes.string.isRequired,
-  };
-  componentDidMount() {
-    window.addEventListener('keydown', this.onCloseModal);
-  }
+export default function Modal({ img, onToggleModal }) {
+  useEffect(() => {
+    const onCloseModal = event => {
+      if (event.code === 'Escape') {
+        onToggleModal();
+      }
+    };
+    window.addEventListener('keydown', onCloseModal);
+    return () => {
+      window.removeEventListener('keydown', onCloseModal);
+    };
+  }, [onToggleModal]);
 
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.onCloseModal);
-  }
-
-  onCloseModal = event => {
-    if (event.code === 'Escape') {
-      this.props.onToggleModal();
-    }
-  };
-
-  onBackdropCloseModal = event => {
+  const onBackdropCloseModal = event => {
     if (event.target === event.currentTarget) {
-      this.props.onToggleModal();
+      onToggleModal();
     }
   };
 
-  render() {
-    return createPortal(
-      <div className={style.Overlay} onClick={this.onBackdropCloseModal}>
-        <div className={style.Modal}>
-          <img src={this.props.img} alt="" />
-        </div>
-      </div>,
-      modalRoot
-    );
-  }
+  return createPortal(
+    <div className={style.Overlay} onClick={onBackdropCloseModal}>
+      <div className={style.Modal}>
+        <img src={img} alt="" />
+      </div>
+    </div>,
+    modalRoot
+  );
 }
 
-// Modal.propTypes = {
-//   onToggleModal: PropTypes.func.isRequired,
-//   img: PropTypes.string.isRequired,
-// };
+Modal.propTypes = {
+  onToggleModal: PropTypes.func.isRequired,
+  img: PropTypes.string.isRequired,
+};
